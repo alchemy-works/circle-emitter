87a8ef0a3aeb0fe42e693933d6a3771d7c2248d3
cbdyzj <cbdyzj@gmail.com>
2024-04-25 15:25:16 +0000

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Circle Emitter
+
+- See: https://github.com/alchem-x/circle-emitter
+
+```
+https://alchemy-works.github.io/circle-emitter/src/main/resources/static/userscript.js
+```
---
+++ src/main/resources/static/userscript.js
@@ -0,0 +1,6 @@
+; (function () {
+    const scriptRef = document.createElement('script');
+    scriptRef.type = 'module'
+    scriptRef.src = 'https://alchem-x.github.io/circle-emitter/src/main/resources/static/userscript.js';
+    document.body.appendChild(scriptRef);
+})();
